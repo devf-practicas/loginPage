b6b5f7446058cfea1a4c6a5e5b58bf8a394c1593
Ever Dávila <github@davila.uno>
2021-08-24 21:43:15 -0500

    validacion de usuario ok

--- js/login.js
+++ js/login.js
@@ -1,11 +1,13 @@
-const user = document.getElementById("user")
-const pass = document.getElementById("pass")
+
 
 function validarDatos(){
+    var user = document.getElementById("user").value
+    var pass = document.getElementById("pass").value
+
     if(user=="prueba" && pass=="123456"){
         alert("ya ingreso con datos ficticios")
     }
     else{
-        alert("Este no es un usuatio valido")
+        alert("Este no es un usuario valido")
     }
 }
