a9b23497934e28c5cd530e893e6f9508cf7ee6ea
caritama <carlosaranda.50@gmail.com>
2021-08-24 20:57:31 -0500

    cambios alert

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const user = document.getElementById("user")
+const pass = document.getElementById("pass")
+
+function validarDatos(){
+    if(user=="prueba" && pass=="123456"){
+        alert("ya ingreso con datos ficticios")
+    }
+    else{
+        alert("Este no es un usuatio valido")
+    }
+}
